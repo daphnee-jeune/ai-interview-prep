e191fa2e95b25db45de6f2bcfeb0cb8c74b6b3df
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-05-02 13:58:57 -0500

    implement routes pt.1

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Page = () => {
+const page = () => {
   return (
     <div>
-      Page
+      SignUp
     </div>
   )
 }
 
-export default Page
+export default page
